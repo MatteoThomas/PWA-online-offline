75fbe8999e33dd424ab31b5263b31181eb0a0ef1
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-07-04 11:48:08 -0500

    Fixed cached .css file path.

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,18 +5,13 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/manifest.webmanifest",
-  "/assets/css/styles.css",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
 // install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
-
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
