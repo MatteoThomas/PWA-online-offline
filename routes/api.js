348a494c990201527c8bd83126221dcfe3f1229b
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-07-04 14:47:07 -0500

    db rename

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
-    .then((budgetDB) => {
-      res.json(budgetDB);
+    .then((budgetdb) => {
+      res.json(budgetdb);
     })
     .catch((err) => {
       res.status(404).json(err);
@@ -13,8 +13,8 @@ router.post("/api/transaction", ({ body }, res) => {
 
 router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then((budgetDB) => {
-      res.json(budgetDB);
+    .then((budgetdb) => {
+      res.json(budgetdb);
     })
     .catch((err) => {
       res.status(404).json(err);
@@ -24,8 +24,8 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
-    .then((budgetDB) => {
-      res.json(budgetDB);
+    .then((budgetdb) => {
+      res.json(budgetdb);
     })
     .catch((err) => {
       res.status(404).json(err);
